a22cb74746483ce9b66a8d72b84d432a523ed411
Vishesh <visheshsingh16@gmail.com>
2017-03-26 15:23:06 -0400

    Angular Test Components

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {MyComponent} from './mycomp.component'
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Hello {{name}}</h1>
+            <mycomp></mycomp>`,
+//  directives: [MyComponent]
+})
+export class AppComponent  { name = 'Angular'; }
---
+++ src/app/mycomp.component.ts
@@ -0,0 +1,33 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'mycomp',
+  template: `<h1>My custom component </h1>
+<!--INTERPOLATION BINDING-->
+            <h2>{{title}}</h2>
+<!--ATTRIBUTE BINDING-->
+            <img [src]="imageLink" />
+            <br>
+<!--BINDING FROM VIEW TO COMPONENT-->
+            <button (click)="runClick()">Click</button><br /><br />
+<!--TWO WAY DATA BINDING-->
+            <label>TWO WAY DATA-BINDING: </label>
+            <input type="text" [(ngModel)]="note"/>
+            {{note}}`
+})
+
+export class MyComponent{
+  private title: string;
+  private imageLink: string;
+  private note: string;
+
+  constructor(){
+    this.title = 'Hello World!';
+    this.imageLink = "http://lorempixel.com/400/300";
+    this.note;
+  }
+
+  runClick(){
+    console.log("You clicked...");
+  }
+}
